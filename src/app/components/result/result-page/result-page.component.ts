83061cd57fcb4dd4563763203e88ac430ae93e36
Andrii Dobrus <58091875+MuffinKing-jpeg@users.noreply.github.com>
2023-06-20 09:12:01 +0300

    Finished result page

--- src/app/components/result/result-page/result-page.component.ts
+++ src/app/components/result/result-page/result-page.component.ts
@@ -1,14 +1,21 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import ContactsJson from '../../../config/contacts.json';
+import Config from '../../../config/conf.json';
 
 @Component({
   selector: 'app-result-page',
   templateUrl: './result-page.component.html',
   styleUrls: ['./result-page.component.scss'],
 })
-export class ResultPageComponent {
+export class ResultPageComponent implements OnInit {
   isSuccess = window.history.state?.['postFormSuccess'];
+  title = this.isSuccess ? Config.companyName + '| Успіх!' : Config.companyName + '| Біда';
   // isSuccess = false;
   errText = window.history.state?.['postFormResponse'];
   protected readonly ContactsJson = ContactsJson;
+  protected readonly window = window;
+
+  ngOnInit() {
+    window.document.title = this.title;
+  }
 }
--- src/app/components/result/result.module.ts
+++ src/app/components/result/result.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {ResultPageComponent} from './success-page/result-page.component';
+import {ResultPageComponent} from './result-page/result-page.component';
 import {RouterModule, Routes} from '@angular/router';
 
 
